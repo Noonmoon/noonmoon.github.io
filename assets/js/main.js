821dea3ac9a3cc88e4a76e59e91a7a680736b231
Noonmoon <tristandeering@hotmail.com>
2019-03-19 17:57:56 -0600

    landing page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,38 @@
+/* LANDING
+/////////////////////////////////////////////*/
+
+$('.title .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.title .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 500,
+    delay: function(el, i) {
+      return 70 * (i+1)
+    }
+  }).add({
+    targets: '.title .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+  }).add({
+    targets: '.sub',
+    scaleX: [0,1],
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 400,
+  })
+/* SLIDER
+/////////////////////////////////////////////*/
+
 var $activeSlide = $('.active'),
 $homeSlide = $(".slide"),
 $slideNavPrev = $("#prev"),
@@ -15,7 +50,7 @@ init();
 
 function disable() {
   ready = false;
-  setTimeout(function() { ready = true; }, 1200)
+  setTimeout(function() { ready = true; }, 1000)
 }
 
 function filterNav(index, activeIndex) {
