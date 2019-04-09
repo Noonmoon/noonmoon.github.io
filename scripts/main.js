e5a532990386d62ef28ff70bf5b2ec17bd5092f1
Noonmoon <tristandeering@hotmail.com>
2019-04-08 22:06:19 -0600

    slider functionality complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,62 +1,75 @@
-let lActive = false;
-var activeSlide = document.querySelector('.active');;
-
 // PORTFOLIO SLIDE
 
 let sliding = false;
 
-function scrollDown(id) {
-  let num = id.substr(id.length - 1)
-  console.log(num)
+function scrollDown(active) {
+  let activeIndex = $(".slide").index(active);
+  let newIndex = $(".slide").index(active.prev());
+  let newNode = document.querySelectorAll(`#slide-${newIndex + 1}`)
+
   anime({
-    targets: activeSlide,
+    targets: ('.active'),
+    top: ['0', '100%'],
+    easing: 'easeOutCubic',
+    duration: 1000
+  })
+  anime({
+    targets: newNode,
     top: ['-100%', '0'],
     easing: 'easeOutCubic',
     duration: 1000
-  });
-  anime({
-    targets: `#slide-2`,
-    top: ['0', '100'],
-    easing: 'easeOutCubic',
-    duration: 1000
-  });
+  })
+  active.removeClass( 'active' )
+  active.prev().addClass('active')
 }
 
-function scrollUp(id) {
-  let num = id.substr(id.length - 1)
-  console.log(num)
+function scrollUp(active) {
+  let activeIndex = $(".slide").index(active);
+  let newIndex = $(".slide").index(active.next());
+  let newNode = document.querySelectorAll(`#slide-${newIndex + 1}`)
+
   anime({
-    targets: activeSlide,
+    targets: ('.active'),
     top: ['0', '-100%'],
     easing: 'easeOutCubic',
     duration: 1000
   })
   anime({
-    targets: `#slide-2`,
+    targets: newNode,
     top: ['100%', '0'],
     easing: 'easeOutCubic',
     duration: 1000
   })
+
+  active.removeClass( 'active' )
+  active.next().addClass('active')
 }
+
 
 $(window).bind('mousewheel', function(event) {
   if (!sliding && event.originalEvent.wheelDelta >= 0) {
-    sliding = true;
-    setTimeout(function() { sliding = false; }, 1100)
+    if ($(".slide").index('.active') !== 0) {
+      sliding = true;
+      setTimeout(function() { sliding = false; }, 1020)
 
-    scrollDown($('.active').attr('id'))
+      scrollDown($('.active'))
+    }
   }
   else if (!sliding && event.originalEvent.wheelDelta < 0) {
-    sliding = true;
-    setTimeout(function() { sliding = false; }, 1100)
+    if ($(".active").nextAll().length !== 0) {
+      sliding = true;
+      setTimeout(function() { sliding = false; }, 1020)
 
-    scrollUp($('.active').attr('id'))
+      scrollUp($('.active'))
+    }
   }
 });
 
 
 
 // ABOUT TRANSITION
+let lActive = false;
+
 $('#contact').click(
   function() {
     if (!lActive) {
@@ -96,6 +109,7 @@ $('#contact').click(
     }
   }
 );
+
 
 // DETAILS TRANSITION
 $('#details').click(
@@ -143,164 +157,6 @@ $('#details').click(
     }
   }
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /* SLIDER
-// /////////////////////////////////////////////*/
-
-// var $activeSlide = $('.active'),
-// $homeSlide = $(".slide"),
-// $slideNavPrev = $("#prev"),
-// $slideNavNext = $("#next"),
-// $nextPrev = $(".prevnext"),
-// $arrow = $(".arrow"),
-// ready = true;
-
-// function init() {
-//   TweenMax.set($homeSlide.not($activeSlide), {autoAlpha: 0});
-//   TweenMax.set($slideNavPrev, {autoAlpha: 0.2});
-// }
-
-// init();
-
-// function disable() {
-//   ready = false;
-//   setTimeout(function() { ready = true; }, 1000)
-// }
-
-// function filterNav(index, activeIndex) {
-//   console.log(index, activeIndex)
-//   if (index === 1 && activeIndex === 0) {
-//     $nextPrev.css("display", "none");
-//     $('.ghost').css("display", "block");
-//     setTimeout(function() {
-//       $('.ghost').css("display", "none");
-//       $nextPrev.css("display", "block");
-//     }, 1000)
-//   } else if (index === 1 && activeIndex > 1) {
-//     $('.ghost').css("display", "none");
-//     $nextPrev.css("display", "block");
-//   }
-//   else if (index === 0) {
-//     $('.ghost').css("display", "block");
-//     $nextPrev.css("display", "none");
-//   } else {
-//     $('.ghost').css("display", "none");
-//     $nextPrev.css("display", "block");
-//   }
-// }
-
-// function goToNextSlide(slideOut, slideIn, slideInAll) {
-//   disable();
-//   filterNav(slideIn.index(), slideOut.index());
-//   var t1 = new TimelineLite(),
-//   slideOutContent = slideOut.find('.content'),
-//   slideInContent = slideIn.find('.content'),
-//   slideOutImg = slideOut.find('.card-img'),
-//   slideInImg = slideIn.find('.card-img'),
-//   index = slideIn.index(),
-//   size = $homeSlide.length;
-
-//   if (slideIn.length !== 0) {
-//     t1.set(slideIn, {autoAlpha: 1, className: '+=active'})
-//       .set(slideOut, {className: '-=active'})
-//       .to(slideOutContent, 0.65, {y: "-=40px", ease: Power3.easeInOut}, 0)
-//       .to(slideOutImg, 0.65, {backgroundPosition: 'bottom', ease: Power3.easeInOut}, 0)
-//       .to(slideInAll, 1, {y: "-=100%", ease: Power3.easeInOut}, 0)
-//       .fromTo(slideInContent, 0.65, {y: '-=40px'}, {y: 0, ease: Power3.easeInOut}, '-=0.60')
-//       .fromTo(slideInImg, 0.65, {backgroundPosition: 'top'}, {backgroundPosition: 'bottom', ease: Power3.easeInOut}, '-=0.7')
-//   }
-
-//   TweenMax.set($slideNavPrev, {autoAlpha: 1});
-
-//   if (index === size - 1) {
-//     TweenMax.to($slideNavNext, 0.3, {autoAlpha: 0.2, ease: Linear.easeNone});
-//   }
-// };
-
-
-// function goToPrevSlide(slideOut, slideIn, slideInAll) {
-//   disable()
-//   filterNav(slideIn.index(), slideOut.index());
-//   var t1 = new TimelineLite(),
-//   slideOutContent = slideOut.find('.content'),
-//   slideInContent = slideIn.find('.content'),
-//   slideOutImg = slideOut.find('.card-img'),
-//   slideInImg = slideIn.find('.card-img'),
-//   index = slideIn.index(),
-//   size = $homeSlide.length;
-
-//   if (slideIn.length !== 0) {
-//     t1.set(slideIn, {autoAlpha: 1, className: '+=active'})
-//       .set(slideOut, {className: '-=active'})
-//       .to(slideOutContent, 0.65, {y: "+=40px", ease: Power3.easeInOut}, 0)
-//       .to(slideOutImg, 0.65, {backgroundPosition: 'top', ease: Power3.easeInOut}, '+=0.7')
-//       .to(slideInAll, 1, {y: "+=100%", ease: Power3.easeInOut}, 0)
-//       .fromTo(slideInContent, 0.65, {y: '+=40px'}, {y: 0, ease: Power3.easeInOut}, 0.65)
-//       .fromTo(slideInImg, 0.65, {backgroundPosition: 'bottom'}, {backgroundPosition: 'top', ease: Power3.easeInOut}, 0)
-//   }
-
-//   TweenMax.set($slideNavNext, {autoAlpha: 1});
-
-//   if (index === size - 1) {
-//     TweenMax.to($slideNavPrev, 0.3, {autoAlpha: 0.2, ease: Linear.easeNone});
-//   }
-// };
-
-
-// $slideNavNext.click(function(e) {
-//   console.log('adfda', $arrow.is(":visible"))
-
-//   e.preventDefault();
-
-//   var slideOut = $('.slide.active'),
-//   slideIn = $('.slide.active').next('.slide'),
-//   slideInAll = $('.slide')
-
-//   if (ready) goToNextSlide(slideOut, slideIn, slideInAll);
-// })
-
-// $arrow.click(function(e) {
-//   e.preventDefault();
-
-//   var slideOut = $('.slide.active'),
-//   slideIn = $('.slide.active').next('.slide'),
-//   slideInAll = $('.slide')
-
-//   if (ready) goToNextSlide(slideOut, slideIn, slideInAll);
-// })
-
-// $slideNavPrev.click(function(e) {
-//   e.preventDefault();
-
-//   var slideOut = $('.slide.active'),
-//   slideIn = $('.slide.active').prev('.slide'),
-//   slideInAll = $('.slide')
-
-//   if (ready) goToPrevSlide(slideOut, slideIn, slideInAll);
-// })
-
-
-
-
-
-
-
-
-
 
 
 
